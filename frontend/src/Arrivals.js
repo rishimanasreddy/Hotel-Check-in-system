b92adff7e7a1085b289c14899f99b23eecc42f8f
rishimanasreddy <rishimanasreddy@gmail.com>
2024-01-31 00:16:09 -0600

    table

--- frontend/src/Arrivals.js
+++ frontend/src/Arrivals.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Data from './data.json'
 
+const d = new Date();
+let date=d.toString()
 function Arrivals() {
   return (
-    <div><h1>Arrivals</h1></div>
+
+    <div>
+    <div><h3>{date}</h3></div>
+
+    <div><table>
+      <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+      </thead>
+      <tbody>
+        {Data.map((d,i)=>(
+          <tr key={i}>
+            <td>{d.ID}</td>
+            <td>{d.Name}</td>
+            <td>{d.Email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table></div>
+
+    </div>
+    
   )
 }
 export default Arrivals;
